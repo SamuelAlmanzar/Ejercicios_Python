b68f123c1319a68d6c0af71e406854c40864452e
Samuel Almanzar <156134580+SamuelAlmanzar@users.noreply.github.com>
2026-01-16 12:45:21 -0400

    08-if.py

---
+++ 08-if.py
@@ -0,0 +1,11 @@
+puntaje = 49
+
+if puntaje >= 95: 
+    print('Aprbado con honores')
+elif puntaje >= 50:
+    print('Alumno aprobado')
+else:
+    print('Alumno reprobado')
+    
+print('Fuera del if')
+    
