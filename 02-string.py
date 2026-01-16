d090b2a7706dabccbc38c2f8e34a9b324ac931ce
Samuel Almanzar <156134580+SamuelAlmanzar@users.noreply.github.com>
2026-01-16 11:48:36 -0400

    02-string.py

---
+++ 02-string.py
@@ -0,0 +1,8 @@
+texto = 'Hola Mundo'
+print(texto.upper())
+print(texto.lower())
+print(texto.find('Mundo'))
+print(texto.find('mundo'))
+nuevoTexto = texto.replace('Mun','Chamaquito Feliz')
+print(texto, nuevoTexto)
+print('Mundo' in texto)
